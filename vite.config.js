ca5d1fdb35d3185f91716d3fec427ef32325f1eb
jacektrefon <jack@trefon.com>
2025-04-06 14:06:32 +0400

    fix: use standard module imports that Vite will properly handle in production

--- vite.config.js
+++ vite.config.js
@@ -29,11 +29,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-      './node_modules/three/build/three.module.js': resolve(__dirname, 'node_modules/three/build/three.module.js'),
-      './node_modules/three/examples/jsm/controls/OrbitControls.js': resolve(__dirname, 'node_modules/three/examples/jsm/controls/OrbitControls.js'),
-      './node_modules/three/examples/jsm/loaders/GLTFLoader.js': resolve(__dirname, 'node_modules/three/examples/jsm/loaders/GLTFLoader.js'),
-      './node_modules/cannon-es/dist/cannon-es.js': resolve(__dirname, 'node_modules/cannon-es/dist/cannon-es.js')
+      '@': resolve(__dirname, './src')
     }
   },
   optimizeDeps: {
